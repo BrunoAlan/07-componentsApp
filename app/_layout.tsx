e3b68374e8a48851c912aabd75a9f91cf5cf5fa8
BrunoAlan <alan.brunoab@gmail.com>
2024-07-27 18:59:33 -0300

    feat: Update RootLayout and HomeScreen components
    This commit updates the RootLayout component in _layout.tsx to include initialRouteName and screenOptions props for better navigation configuration. It also updates the HomeScreen component in HomeScreen.tsx to include the Icon component, allowing the rendering of icons using the Ionicons library. This enhances the visual appeal and functionality of the HomeScreen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,12 @@ import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      initialRouteName='index'
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name='index' />
     </Stack>
   );
--- src/presentations/screens/home/HomeScreen.tsx
+++ src/presentations/screens/home/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import Icon from '../../icons/Icon';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>HomeScreen</Text>
+      <Icon name='add' color='blue' size={24} />
     </View>
   );
 };
