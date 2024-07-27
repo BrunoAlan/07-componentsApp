18f8a0e4cff85dbaf1b0f819115b2231a73cc5d3
BrunoAlan <alan.brunoab@gmail.com>
2024-07-27 19:35:31 -0300

    refactor: Update Icon component to spread rest props

--- src/presentations/icons/Icon.tsx
+++ src/presentations/icons/Icon.tsx
@@ -1,14 +1,22 @@
+import { colors } from '@/src/config/theme';
 import { Ionicons } from '@expo/vector-icons';
+import { StyleProp, TextStyle } from 'react-native';
 
 interface Props {
-  name: keyof typeof Ionicons.glyphMap;
-  color: string;
-  size: number;
-  style?: object;
+  name: keyof typeof Ionicons.defaultProps;
+  color?: string;
+  size?: number;
+  style?: StyleProp<TextStyle>;
   rest?: any;
 }
 
-const Icon = ({ name, color, size, style, ...rest }: Props) => {
+const Icon = ({
+  name,
+  color = colors.text,
+  size = 24,
+  style,
+  ...rest
+}: Props) => {
   return (
     <Ionicons name={name} color={color} size={size} style={style} {...rest} />
   );
