9aec58aecbae0a9f5349a9f7074b1c3a5e050a7a
BrunoAlan <alan.brunoab@gmail.com>
2024-07-27 19:36:06 -0300

    feat: Add global styles and menu items to HomeScreen
    This commit adds global styles from the `theme` module to the `HomeScreen` component in `HomeScreen.tsx`. It also includes an array of menu items to be rendered in the `ScrollView` component. This enhances the visual appeal and functionality of the HomeScreen by applying consistent styling and providing a dynamic menu system.

--- src/presentations/screens/home/HomeScreen.tsx
+++ src/presentations/screens/home/HomeScreen.tsx
@@ -1,11 +1,83 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import Icon from '../../icons/Icon';
+import { globalStyles } from '@/src/config/theme';
+import Title from '../../components/ui/Title';
+import MenuItem from '../../components/ui/MenuItem';
+
+export const menuItems = [
+  // 01-animationMenuItems
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen',
+  },
+
+  // 02-menuItems
+  {
+    name: 'Pull to refresh',
+    icon: 'refresh-outline',
+    component: 'PullToRefreshScreen',
+  },
+  {
+    name: 'Section List',
+    icon: 'list-outline',
+    component: 'CustomSectionListScreen',
+  },
+  {
+    name: 'Modal',
+    icon: 'copy-outline',
+    component: 'ModalScreen',
+  },
+  {
+    name: 'InfiniteScroll',
+    icon: 'download-outline',
+    component: 'InfiniteScrollScreen',
+  },
+  {
+    name: 'Slides',
+    icon: 'flower-outline',
+    component: 'SlidesScreen',
+  },
+  {
+    name: 'Themes',
+    icon: 'flask-outline',
+    component: 'ChangeThemeScreen',
+  },
+
+  // 03- uiMenuItems
+  {
+    name: 'Switches',
+    icon: 'toggle-outline',
+    component: 'SwitchScreen',
+  },
+  {
+    name: 'Alerts',
+    icon: 'alert-circle-outline',
+    component: 'AlertScreen',
+  },
+  {
+    name: 'TextInputs',
+    icon: 'document-text-outline',
+    component: 'TextInputScreen',
+  },
+];
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>HomeScreen</Text>
-      <Icon name='add' color='blue' size={24} />
+    <View style={globalStyles.mainContainer}>
+      <View style={globalStyles.globalMargin}>
+        <ScrollView>
+          <Title safe text='Menu options' />
+          {menuItems.map((item, index) => (
+            <MenuItem key={index} {...item} />
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 };
